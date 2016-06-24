7840ae7292b51a7d01462d05b3d854f6f34cd057
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2016-06-24 14:44:22 +0200

    fixes

--- css-micro-parser.h
+++ css-micro-parser.h
@@ -2,10 +2,11 @@
 
 #pragma once
 
-//#include <css-layout.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
+#include <css-layout.h>
 
 bool css_parse_is_ws(char c) {return c == ' ' || c == '\n' || c == '\r';}
 bool css_parse_is_fsep(char c) {return c == ':' || c == '\0' || css_parse_is_ws(c);}
@@ -15,22 +16,22 @@ size_t css_skip_ws(const char * t, size_t i)
 	while(css_parse_is_ws(t[i])) ++i;
 	return i;
 }
-void css_parse_shuffle6(float * values, float * new_values)
+void css_parse_shuffle6(float * values, float * new_values, uint8_t count)
 {
 	// css provide top right bottom left start end
 	for(size_t i = 0; i < 6; ++i)
-		new_values[i] = NAN;
+		new_values[i] = 0.0f;
 
-	if(values[0] == NAN)
+	if(count == 0)
 		return;
-	else if(values[1] == NAN)
+	else if(count == 1)
 		new_values[CSS_LEFT] = new_values[CSS_RIGHT] = new_values[CSS_BOTTOM] = new_values[CSS_TOP] = values[0];
-	else if(values[2] == NAN)
+	else if(count == 2)
 	{
 		new_values[CSS_BOTTOM] = new_values[CSS_TOP] = values[0];
 		new_values[CSS_LEFT] = new_values[CSS_RIGHT] = values[1];
 	}
-	else if(values[3] == NAN)
+	else if(count == 3)
 	{
 		new_values[CSS_TOP] = values[0];
 		new_values[CSS_LEFT] = new_values[CSS_RIGHT] = values[1];
@@ -42,8 +43,19 @@ void css_parse_shuffle6(float * values, float * new_values)
 		new_values[CSS_RIGHT] = values[1];
 		new_values[CSS_BOTTOM] = values[2];
 		new_values[CSS_LEFT] = values[3];
-		new_values[CSS_START] = values[4];
-		new_values[CSS_END] = values[5];
+		new_values[CSS_START] = count > 4 ? values[4] : 0.0f;
+		new_values[CSS_END] = count > 5 ? values[5] : 0.0f;
+	}
+}
+void css_parse_set_param(void * ptr, uint8_t size, uint64_t val)
+{
+	switch(size) // this is needed to support bigend things
+	{
+	case 1: *(uint8_t*) ptr = val; break;
+	case 2: *(uint16_t*)ptr = val; break;
+	case 4: *(uint32_t*)ptr = val; break;
+	case 8: *(uint64_t*)ptr = val; break;
+	default: assert(0); break;
 	}
 }
 css_style_t css_parse(const char * style)
@@ -157,6 +169,15 @@ css_style_t css_parse(const char * style)
 	};
 	size_t param_count = sizeof(params) / sizeof(param_t);
 
+	// set defaults
+	for(size_t n = 0; n < param_count; ++n)
+		for(size_t m = 0; m < 6; ++m)
+			if(!params[n].values[m].key)
+			{
+				css_parse_set_param(params[n].field, params[n].size, params[n].values[m].val);
+				break;
+			}
+
 	char field[32] = {0};
 	char value[6][32] = {0};
 
@@ -168,8 +189,14 @@ css_style_t css_parse(const char * style)
 
 		i = css_skip_ws(style, i);
 
+		if(style[i] == '\0')
+			continue;
+
 		while(!css_parse_is_fsep(style[i]))
+		{
+			assert(j < 32 && "max css style field size is 32 characters");
 			field[j++] = style[i++];
+		}
 		j = 0;
 
 		i = css_skip_ws(style, i);
@@ -179,8 +206,12 @@ css_style_t css_parse(const char * style)
 
 		while(style[i] != ';' && style[i] != '\0')
 		{
+			assert(k < 6 && "only 6 values are possible in one css style field");
 			while(!css_parse_is_vsep(style[i]))
+			{
+				assert(j < 32 && "max css style value size is 32 characters");
 				value[k][j++] = style[i++];
+			}
 			k++;
 			j = 0;
 			i = css_skip_ws(style, i);
@@ -193,42 +224,47 @@ css_style_t css_parse(const char * style)
 		for(size_t n = 0; n < param_count; ++n)
 		{
 			if(!strcmp(field, params[n].param))
+			{
 				for(size_t m = 0; m < 6; ++m)
-					if(!params[n].values[m].key || !strcmp(value[0], params[n].values[m].key))
+					if(params[n].values[m].key && !strcmp(value[0], params[n].values[m].key))
 					{
-						switch(params[n].size) // this is needed to support bigend things
-						{
-						case 1: *(uint8_t*)params[n].field = params[n].values[m].val; break;
-						case 2: *(uint16_t*)params[n].field = params[n].values[m].val; break;
-						case 4: *(uint32_t*)params[n].field = params[n].values[m].val; break;
-						case 8: *(uint64_t*)params[n].field = params[n].values[m].val; break;
-						default: assert(0); break;
-						}
+						css_parse_set_param(params[n].field, params[n].size, params[n].values[m].val);
 						set = true;
 						break;
 					}
+				if(!set)
+				{
+					printf("ignoring unknown value '%s' for css style field '%s'\n", value[0], field);
+					set = true; // ignore this field
+				}
+			}
 			if(set)
 				break;
 		}
 
 		if(!set)
 		{
-			float num_value[6] = {0.0f};
 			// we know that all following values are numbers
+			float num_value[6] = {0.0f};
+			uint8_t count = 6;
 			for(size_t n = 0; n < 6; ++n)
 			{
 				if(strlen(value[n]))
-					// TODO support px, %, etc, auto for NAN
+					// TODO support px, %, etc, auto
 					num_value[n] = atof(value[n]);
 				else
-					num_value[n] = NAN;
+				{
+					count = n;
+					num_value[n] = 0.0f;
+					break;
+				}
 			}
 
 			if(!strcmp(field, "flex"))				r.flex = num_value[0];
-			else if(!strcmp(field, "margin"))		css_parse_shuffle6(num_value, r.margin);
-			else if(!strcmp(field, "padding"))		css_parse_shuffle6(num_value, r.padding);
-			else if(!strcmp(field, "border"))		css_parse_shuffle6(num_value, r.border);
-			//else if(!strcmp(field, "border-radius"))r.border_radius = num_value[0]; // if you have it :)
+			else if(!strcmp(field, "margin"))		css_parse_shuffle6(num_value, r.margin, count);
+			else if(!strcmp(field, "padding"))		css_parse_shuffle6(num_value, r.padding, count);
+			else if(!strcmp(field, "border"))		css_parse_shuffle6(num_value, r.border, count);
+			else if(!strcmp(field, "border-radius"))r.border_radius = num_value[0]; // if you have it :)
 			else if(!strcmp(field, "top"))			r.position[CSS_TOP] = num_value[0];
 			else if(!strcmp(field, "bottom"))		r.position[CSS_BOTTOM] = num_value[0];
 			else if(!strcmp(field, "left"))			r.position[CSS_LEFT] = num_value[0];
@@ -239,6 +275,10 @@ css_style_t css_parse(const char * style)
 			else if(!strcmp(field, "max-height"))	r.maxDimensions[CSS_HEIGHT] = num_value[0];
 			else if(!strcmp(field, "min-width"))	r.minDimensions[CSS_WIDTH] = num_value[0];
 			else if(!strcmp(field, "min-height"))	r.minDimensions[CSS_HEIGHT] = num_value[0];
+			else
+			{
+				printf("unknown css style field '%s'\n", field);
+			}
 		}
 	}
 
